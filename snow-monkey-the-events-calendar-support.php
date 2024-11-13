e96c386bbd41028cfec62f2c355632c0b494cff9
inc2734 <inc@2inc.org>
2024-11-13 19:00:31 +0900

    0.4.4

--- snow-monkey-the-events-calendar-support.php
+++ snow-monkey-the-events-calendar-support.php
@@ -2,7 +2,7 @@
 /**
  * Plugin name: Snow Monkey The Events Calendar Integrator
  * Description: With this plugin, Snow Monkey can use The Events Calendar plugin.
- * Version: 0.4.3
+ * Version: 0.4.4
  * Tested up to: 6.7
  * Requires at least: 6.7
  * Requires PHP: 7.4
